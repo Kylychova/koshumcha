5da7c36a01a3254ffe25fbb66aedd5d9b91f27b3
Aida Kylychova <akylychova@MBP-Aida.lan>
2021-12-01 20:04:27 +0600

    initial commit

---
+++ koshumcha/koshumchaApp.swift
@@ -0,0 +1,17 @@
+//
+//  koshumchaApp.swift
+//  koshumcha
+//
+//  Created by Aida Kylychova on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct koshumchaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
